bc2ad1e3f6676547c1d075cca572c30a859fb3cb
Fatou-Maty Diouf <fatou@mamafatou.com>
2022-08-02 17:44:14 +0200

    MapView camera setup

--- Assets/Scripts/MapView.cs
+++ Assets/Scripts/MapView.cs
@@ -12,15 +12,17 @@ using UnityEngine;
 
 public class MapView : MonoBehaviour
 {
-    [SerializeField] Vector3 cameraTransformOffset; //Camera offset from player
-    [SerializeField] Vector3 cameraRotationOffset; //Camera x-axis tilt
+    [SerializeField] Vector3 cameraPosition; //Camera offset from player
+    [SerializeField] Vector3 cameraRotation = new Vector3(90, 0, 0); //Camera x-axis tilt
     private GameObject player;
     [SerializeField] int mapView = 1;
+    [SerializeField] int size;
 
 
     void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        transform.rotation = Quaternion.Euler(cameraRotation);
         SetCameraView(2);
     }
 
@@ -29,24 +31,19 @@ public class MapView : MonoBehaviour
     {
         if (mapView == 1)
         {
-            cameraTransformOffset = new Vector3(168, 370, 47);
-            cameraRotationOffset = new Vector3(90, 0, 0);
+            cameraPosition = new Vector3(120, 1, 120);
+            size = 130;
         }
         else if (mapView == 2)
         {
-            cameraTransformOffset = new Vector3(168, 244, 121);
-            cameraRotationOffset = new Vector3(90, 0, 0);
+            cameraPosition = new Vector3(250, 1, -17.5f);
+            size = 130;
         }
         else if (mapView == 3)
         {
-            cameraTransformOffset = new Vector3(245, 244, -16);
-            cameraRotationOffset = new Vector3(90, 0, 0);
+            cameraPosition = new Vector3(200, 1, 52.5f);
+            size = 200;
         }
-    }
-
-    void LateUpdate()
-    {
-        transform.position = player.transform.position + cameraTransformOffset; // Sets camera to player movement + offset
-        transform.rotation = Quaternion.Euler(cameraRotationOffset + player.transform.eulerAngles); // Sets camera to 
+        transform.position = cameraPosition; // Sets camera to player movement + offset
     }
 }
